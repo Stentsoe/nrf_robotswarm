644a5b149fa90a35eac5b6e41b1ef6ca1fb65c91
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2022-06-16 16:51:34 +0200

    Update API to use 32 bit ints
    Signed-off-by: Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>

--- samples/app_event_manager/drivers/motors/motor.h
+++ samples/app_event_manager/drivers/motors/motor.h
@@ -2,9 +2,10 @@
 #include <zephyr.h>
 #include <device.h>
 
-typedef int (*drive_continous_t)(const struct device *dev, int16_t power);
+typedef int (*drive_continous_t)(const struct device *dev, int32_t power);
 
-typedef int (*set_position_t)(const struct device *dev, int position, int16_t power, bool hold);
+typedef int (*set_position_t)(const struct device *dev, int position, int32_t power, bool hold);
+
 
 struct motor_api
 {
@@ -17,14 +18,18 @@ struct motor_api
  *
  * @param dev Motor device
  * @param power Power of the motor. Negative values give same power in oposite direction.
+ *              Relationship between power parameter and actual output from the motor depends
+ *              on the underlying driver
  * @return 0 on success, negative errno code otherwise.
  *         -ENOTSUP if motor does not support continous rotation. 
  *         Other error codes are defined by the underlying driver.
  */
-static inline int drive_continous(const struct device *dev, int16_t power)
+static inline int drive_continous(const struct device *dev, int32_t power)
 {
     const struct motor_api *api = (struct motor_api *)dev->api;
-
+    if (api->drive_continous == NULL){
+        return -ENOTSUP;
+    }
     return api->drive_continous(dev, power);
 }
 
@@ -34,15 +39,21 @@ static inline int drive_continous(const struct device *dev, int16_t power)
  * @param dev Motor device
  * @param position Position the motor shoul be moved to.
  * @param power Power with which to move the motor. Negative values move the motor towards 
+ *              Relationship between power parameter and actual output from the motor depends
+ *              on the underlying driver
  * @param hold Attempts to hold position when reached if true. Powers off motor if false.
  * @return 0 on success, negative errno code otherwise.
  *         -ENOTSUP if motor does not support moving to a specific position.
  *         -ENOTSUP if hold == true and the motor does not support holding the motor position.
  *         Other error codes are defined by the underlying driver.
  */
-static inline int set_position(const struct device *dev, int16_t position, int16_t power, bool hold)
+static inline int set_position(const struct device *dev, int32_t position, int32_t power, bool hold)
 {
     const struct motor_api *api = (struct motor_api *)dev->api;
+
+    if (api->set_position == NULL){
+        return -ENOTSUP;
+    }
 
     return api->set_position(dev, position, power, hold);
 }
